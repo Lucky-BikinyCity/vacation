0dff1e4b2f9e22d0c7741471e7e216038fda5827
tjdalsdl1349 <tjdalsdl1349@naver.com>
2024-07-14 22:17:18 +0900

    main page - add hover css on Profile.

--- my-webpage/frontend/src/index.js
+++ my-webpage/frontend/src/index.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     if(document.body.id==='loginBody'){ //body태그 id를 이용해서 각 html파일별로 필요 js 함수만을 사용
         LoginBody();
+    }else if(document.body.id==='signUpBody'){
+        SignUpBody();
     }
 });
 
@@ -9,9 +11,61 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //로그인 페이지
 function LoginBody(){
+    showPwdInLogin();
 
+    delInputContent()
 }
 
+function SignUpBody(){
+    showPwdInSignUp();
+}
+
+    //로그인 페이지에서 비밀번호 표시 on/off 기능
+function showPwdInLogin(){
+    var checkbox = document.getElementById("showPwd");
+    var pwd = document.getElementById("password");
+
+    checkbox.addEventListener("change",function(){
+        if(checkbox.checked){
+            pwd.type="text";
+        }else{
+            pwd.type="password";
+        }
+    });
+}
+
+    //로그인 input 내용 지우기 기능
+function delInputContent(){
+    var delIdInputContent = document.getElementById('delIdInputContent');
+    var delPwdInputContent = document.getElementById('delPwdInputContent');
+    var idInput = document.getElementById('uid');
+    var pwdInput = document.getElementById('password');
+    
+    delIdInputContent.addEventListener('click',function(){
+        idInput.value='';
+    })
+
+    delPwdInputContent.addEventListener('click',function(){
+        pwdInput.value='';
+    })
+}
+
+    //회원가입 페이지에서 비밀번호 표시 on/off 기능
+function showPwdInSignUp(){
+    var checkbox = document.getElementById("showPwdInSignUp");
+    var img = document.getElementById("imgOfLabelOfshowPwdInSignUp");
+    var pwd = document.getElementById("password");
+
+    checkbox.addEventListener("change",function(){
+        if(checkbox.checked){
+            pwd.type="text";
+            img.src="../imgs/icon/showCode.png";
+        }else{
+            pwd.type="password";
+            img.src="../imgs/icon/hideCode.png";
+        }
+    });
+}
 
 
 //이 아래서부턴 필요한 js함수 작성
