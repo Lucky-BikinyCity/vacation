9fc210c2f3cdaf3a860448428d8833eeef948d49
eating2 <napo987600@gmail.com>
2024-07-15 14:53:53 +0900

    build : dotenv 설치

--- my-webpage/server/app.js
+++ my-webpage/server/app.js
@@ -35,53 +35,53 @@ app.get('/', (req, res) => {
 
 //회원가입
 app.post('/signup', (req, res) => {
-    const { ID, PW, name } = req.body;
-    const sql = 'INSERT INTO users (ID, PW, name) VALUES (?, ?, ?)';
-    db.query(sql, [ID, PW, name], (err, results) =>{
+    const { ID, PW, username } = req.body;
+    const sql = 'INSERT INTO User (user_ID, password, user_name) VALUES (?, ?, ?)';
+    db.query(sql, [ID, PW, username], (err, results) =>{
         if(err){
             return res.status(500).send(err);
         }
-        res.json({ id: results.insertId, name, email});
+        res.json({ id: results.insertId, username });
     });
 });
 
 //로그인
 app.post('/login', (req, res) => {
-    const { ID, PW } = req.body;
-  
-    if (!ID || !PW) {
-      return res.status(400).json({ message: 'ID와 PW를 입력해주세요' });
+  const { ID, PW } = req.body;
+
+  if (!ID || !PW) {
+    return res.status(400).json({ message: 'ID와 PW를 입력해주세요' });
+  }
+
+  const query = 'SELECT * FROM User WHERE user_ID = ?';
+  db.query(query, [ID], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: '서버 오류' });
     }
-  
-    const query = 'SELECT * FROM users WHERE ID = ?';
-    db.query(query, [ID], (err, results) => {
+
+    if (results.length === 0) {
+      return res.status(401).json({ message: 'ID 또는 PW가 잘못되었습니다' });
+    }
+
+    const user = results[0];
+
+    bcrypt.compare(PW, user.PW, (err, isMatch) => {
       if (err) {
         return res.status(500).json({ message: '서버 오류' });
       }
-  
-      if (results.length === 0) {
+
+      if (!isMatch) {
         return res.status(401).json({ message: 'ID 또는 PW가 잘못되었습니다' });
       }
-  
-      const user = results[0];
-  
-      bcrypt.compare(PW, user.PW, (err, isMatch) => {
-        if (err) {
-          return res.status(500).json({ message: '서버 오류' });
-        }
-  
-        if (!isMatch) {
-          return res.status(401).json({ message: 'ID 또는 PW가 잘못되었습니다' });
-        }
-  
-        const token = jwt.sign({ id: user.ID }, 'your_jwt_secret', {
-          expiresIn: '1h'
-        });
-  
-        res.json({ message: '로그인 성공', token });
+
+      const token = jwt.sign({ id: user.ID }, 'your_jwt_secret', {
+        expiresIn: '1h'
       });
+
+      res.json({ message: '로그인 성공', token });
     });
   });
+});
   
 
 //서버 호출 정보 - 몇 번 포트에서 실행되었습니다.
